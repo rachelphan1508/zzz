dff35994acf7b3378e8b2fcdf8a79a5c83bb9220
rachelphan1508 <49543269+rachelphan1508@users.noreply.github.com>
2019-04-12 18:56:27 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,27 +13,9 @@ using namespace std;
 int main()
 {
     List<Book> Library;
-    vector <string> users;
-    users.resize(0);
-    //get the ID information
-    cout << "How many users?" << endl;
-    int num_user;
-    cin >> num_user;
-    for (int i=0;i<num_user;i++) {
-        cout << "Enter your ID" << endl;
-        string ID;
-        cin >> ID;
-        users.push_back(ID);
-        List<Book> users[i];
-    }
-        //Library.Insert_User(ID);
-    
     cout << "How many books do the library have? ";
     int num;
     cin >> num;
-
-    
-    
     for (int i=0;i < num;i++ ) {
         
         cout << "Enter the book name: " << endl ;
@@ -50,6 +32,14 @@ int main()
         Library.Insert(mybook);
         
     }
+
+    string more_user = "y";
+    
+    while (more_user == "y" ){
+    cout << "Enter your ID" << endl;
+    string ID;
+    cin >> ID;
+    
     string again = "y";
     int answer;
     string title;
@@ -79,13 +69,16 @@ int main()
                 cout << "The book has not been checked out yet. " << endl;
             }
             else {
-                cout << "You have successfully returned the " << title << " book.";
+                cout << "You have successfully returned the " << title <<
+                        " book." << endl;
             }
         }
         if (answer == 3) {
+            cout << "List of books that you checked out: " << endl;
             Library.Print_Checked();
         }
         if (answer ==4) {
+            cout << "Here's the books and quantity." << endl;
             Library.Print_Library();
         }
         cout << "Continue? (y for yes)" << endl;
@@ -94,11 +87,9 @@ int main()
         
         
     }
-
-    //set_title and quantity to Book
-    //insert Book to List
-    
-    //give user a menu of option
+    cout << "Another user? (y for yes)" ;
+    cin >> more_user;
+    }
     
     
     return 0;
