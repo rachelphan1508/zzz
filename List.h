519b2a46c2daa7934e14a51e114dbeaf287c5662
rachelphan1508 <49543269+rachelphan1508@users.noreply.github.com>
2019-04-12 18:56:50 -0400

    Update List.h

--- List.h
+++ List.h
@@ -28,12 +28,14 @@ class List{
         void Insert(const T& new_item);
         void Print_Library() const;
         void Print_Checked();
+        void  Insert_Checked(const T& new_item);
 
   private:
       
       //vector<string> users;
       Node<T>* Checked_Out;
       Node<T>* BookList;
+      
               
             
 };
@@ -139,43 +141,8 @@ Book List<T> :: Get_Book(string title) {
 //check out a book
 template <typename T>
 bool List<T> ::Check_Out(const string& title) {
-        Node<T>* orig_list_pos = BookList; // holds the position of
-	                                 // the thing we're copying
-	                                 // in L
-	if(orig_list_pos == NULL) // then L is empty..
-	  BookList = NULL; //...and so are we
-	else{
-	  BookList = new Node<T>;
-	  BookList->set_item(orig_list_pos->get_item());
-	  Node<T>* new_list_end= BookList; // points to the last item in the
-	                      // list we're creating
-	  orig_list_pos = orig_list_pos->get_next();
-	  
-	  // this loop will continually copy items from the original
-	  // list (pointed at by "orig_list_pos") to the new list
-	  // (whose last item is pointed at by "new_list_end"), until
-	  // the original list runs out of elements
-	  while(orig_list_pos != NULL) {
-	  	// the loop has 2 parts.  First, we create the new node,
-	  	// and set it's value
-	  	
-	  	Node<T>* n = new Node<T>;
-	  	n->set_item(orig_list_pos->get_item());
-	  	n->set_next(NULL);
-	  	orig_list_pos->set_next(n); // links n into the list
-//	  	
-	  	// now, we set up the next iteration
-	  	new_list_end = n; //so it points to the last element in
-	  	                   // the new list again
-	  	orig_list_pos =new_list_end->get_next();
-	  }
-	  
-	  // when we leave the loop, we have a whole new copy, 
-	  // pointed at by theList.  The only thing we have to do
-	  // is set the last element to point to NULL.
-	  new_list_end->set_next(NULL);
-	}
-    //Node<T>* p = BookList;
+        
+    Node<T>* orig_list_pos = BookList;
     //List<T>* n = new List;
     //n->Insert(p->get_item());
     //n->set_next(NULL);
@@ -186,45 +153,35 @@ bool List<T> ::Check_Out(const string& title) {
             }
             else {
                 orig_list_pos->get_item().Reduce_Quan();
-                
-                //Checked_Out->set_item(p->get_item());
+                Insert_Checked(orig_list_pos->get_item());
                 return true;
             }
                 
         }
         orig_list_pos = orig_list_pos->get_next();
     }
+    return false;
 
 }
-//check if the book has been checked out or not
-template <typename T>
-bool List<T>:: Is_Book_Checked(Book thebook) {
-    Node<T>* p = Checked_Out;
-    while(p!= NULL ) {
-        if (p->get_item().get_title() == thebook.get_title()) {
-            return true;
-        }
-        p = p->get_next();
-        
-    }
-    return false;
-}
+
 //return a book
 template <typename T>
 bool List<T> ::Check_In(const string& title) {
     Node<T>* p = BookList;
-    Book thebook = Get_Book(title);
-    if (!Is_Book_Checked(thebook)) {
-        
-        return false;
-        
+    Node<T>* n = Checked_Out;
+    while(n!= NULL ) {
+        if (n->get_item().get_title() == n->get_item().get_title()) {
+            while(p!=NULL) {
+                if (p->get_item().get_title() == n->get_item().get_title()) {
+                    p->get_item().Increase_Quan();
+                }
+                p = p->get_next();
+            }
+            return true;
+        }
+        n = n->get_next();
     }
-    else {
-        thebook.Increase_Quan();        
-        cout << "You have successfully checked in the " << title << " book."
-                << endl;
-        return true;
-    }
+    return false;
 }
 
 
@@ -241,19 +198,32 @@ void List<T> :: Print_Checked() {
     
 } 
 
-
 template <typename T>
-void List<T>:: DeepCopy(Node<T>* p, Node<T>* &copy){
-	while (p != NULL)
-	{
-		{
-			Node<T>* copy = new Node<T>;
-			copy->set_item(p->get_item());
-			copy->set_next(p->get_next());
-			p = p->get_next();
-		}
-	}
+void List<T>:: Insert_Checked(const T& new_item) {
+    Node<T>* p = Checked_Out;
+    Node<T>* n = new Node<T>;
+    Node<T>* q;
+    n-> set_item(new_item);
+    if (p==NULL ){
+        Checked_Out = n;
+    }
+    else {
+        q = p->get_next();
+        if (p->get_item().get_title() > n->get_item().get_title()) {
+            n->set_next(p);
+            Checked_Out = n;
+        }
+        else {
+            while (q!= NULL && q->get_item().get_title() < n->get_item().get_title()) {
+                //stop the first time q->get_item() > n->get_item()
+                p = q;
+                q = q->get_next();//get to the next q
+            }
+            p->set_next(n);
+            n->set_next(q);
+        }
+        
+    }    
 }
-
 
 #endif
